3b8f1da13cd96a075fba6c45015ab311f9218b36
Maryana <ilon.mars@yandex.ru>
2023-04-15 01:48:50 +0300

    update: error screen ✨

--- src/components/AppError.vue
+++ src/components/AppError.vue
@@ -1,3 +1,41 @@
 <template>
-  <slot />
+  <div class="loading-error">
+    <span class="loading-error__error" data-text="error">Error</span>
+    <span class="loading-error__message"><slot /></span>
+  </div>
 </template>
+
+<style lang="sass">
+.loading-error
+  width: 100%
+  height: 100vh
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+
+  &__error
+    +ui-space(bottom, 4)
+    position: relative
+    color: $light-color
+    font-size: rem($ui-step-outer * 5)
+    letter-spacing: .5em
+    text-transform: uppercase
+    animation: glitch-main 1s infinite linear alternate-reverse
+
+    &::before
+      +glitchCopy
+      left: 2px
+      text-shadow: -2px 0 #ff00c1
+      clip: rect(44px, 450px, 56px, 0)
+      animation: glitch-before 5s infinite linear alternate-reverse
+
+    &::after
+      +glitchCopy
+      left: -2px
+      text-shadow: -2px 0 #00fff9, 2px 2px #ff00c1
+      animation: glitch-after 1s infinite linear alternate-reverse
+
+  &__message
+    color: #ff00c1
+</style>
